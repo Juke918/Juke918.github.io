ce9bd193629182559db8068b6d6b251f39befce1
Juke <kennedyj21@bcdschool.org>
2020-01-19 21:37:11 -0500

    working on automating git

--- write.py
+++ write.py
@@ -1,17 +1,23 @@
+import os
+
 def main():
 
 	
 	w= open("index.html", "a+")
 	
-	name = input("What is your name?")
-
-	w.write(name)	
-
-	prop = input("Given all the information you've learned, what system might you propose?")
+	name = input("What is your name?\n")
 	
-	w.write(prop)
+
+	w.write("\n" + name + "\n")
+	
+
+	prop = input("Given all the information you've learned, what system might you propose? \n")
+	
+	w.write(prop + "\n")
 	
 	w.close()
+
+	os.system("git push --all")
 
 if __name__== "__main__":
 	
